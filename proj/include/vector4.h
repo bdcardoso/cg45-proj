24b6fc734af2260cf54eb533e2b45e7d462b5da1
Rodrigo Lourenço <guigo.lourenco@gmail.com>
2014-09-23 20:03:15 +0100

    Started to implement vector4

--- proj/include/vector4.h
+++ proj/include/vector4.h
@@ -1,30 +1,33 @@
 #pragma once
 #include "vector3.h"
+#include <GL/gl.h>
 
 class vector4 {
-    double _x, _y, _z, _w;
+    // [0] -> x, [1] -> y, [2] -> z, [3] -> w
+    GLdouble _coords[4];
 
 public:
     vector4();
     vector4(const vector3 &in);
-    vector4(double x, double y, double z, double w);
+    vector4(GLdouble x, GLdouble y, GLdouble z, GLdouble w);
 
-    double x();
-    double y();
-    double z();
-    double w();
+    GLdouble x();
+    GLdouble y();
+    GLdouble z();
+    GLdouble w();
 
-    void x(double newx);
-    void y(double newy);
-    void z(double newz);
-    void w(double neww);
+    void x(GLdouble newx);
+    void y(GLdouble newy);
+    void z(GLdouble newz);
+    void w(GLdouble neww);
 
-    void set(double x, double y, double z, double w);
+    const GLdouble *get() const;
+    void set(GLdouble x, GLdouble y, GLdouble z, GLdouble w);
     
     vector4 &operator+(const vector4 &rhs);
     vector4 &operator+(const vector3 &rhs);
     vector4 &operator-(const vector4 &rhs);
     vector4 &operator-(const vector3 &rhs);
-    vector4 &operator*(double scalar);
+    vector4 &operator*(GLdouble scalar);
 };
 
