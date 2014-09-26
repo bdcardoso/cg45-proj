a50d18a58ab9e955bc5051dbf5a60d025a452c10
Rodrigo Lourenço <guigo.lourenco@gmail.com>
2014-09-26 16:56:20 +0100

    Added vector4 implementation.

---
+++ proj/src/vector4.cpp
@@ -0,0 +1,30 @@
+#include "vector4.h"
+
+vector4::vector4() : vector4(0, 0, 0, 0) {
+}
+
+vector4::vector4(const vector3 &in) : vector4(in.x(), in.y(), in.z(), 0) {
+}
+
+vector4::vector4(GLdouble x, GLdouble y, GLdouble z, GLdouble w) {
+    _coords[0] = x;
+    _coords[1] = y;
+    _coords[2] = z;
+    _coords[3] = w;
+}
+
+GLdouble vector4::x() {
+    return _coords[0];
+}
+
+GLdouble vector4::y() {
+    return _coords[1];
+}
+
+GLdouble vector4::z() {
+    return _coords[2];
+}
+
+GLdouble vector4::w() {
+    return _coords[3];
+}
