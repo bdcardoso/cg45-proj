e7b86e4e6627668fda3ccb5670b785e93add2bff
VonWeidtfuderr <luis.ribeiro.gomes@tecnico.ulisboa.pt>
2014-10-05 16:24:59 +0100

    Pelo desenho do Bruno, não percebi se devo desenhar o corpo do carro com glBegin ou com glutSolidCube. De resto, não sei se o carro "encolhe" quando aparece no jogo ou se mantém o tamanho.

--- proj/src/car.cpp
+++ proj/src/car.cpp
@@ -1,18 +1,62 @@
 #include "car.h"
 
 void car::draw() {
-    // Frame
-    glColor3ub(255, 0, 0);
+    //Lower Body
     glPushMatrix();
-    glScalef(4.0, 0.4, 1.5);
-    glutSolidCube(1.0);
+    glTranslatef(0.0f, -0.08f, 0.0f);
+    glScalef(0.78125f, 0.16f, 0.0f);
+    glPushMatrix();
+    glColor3ub(0, 0, 255);
+    glTranslatef(1.0f, 1.0f, 0.0f);
+    glutSolidCube(1);
+    glPopMatrix();
     glPopMatrix();
 
-    glColor3ub(0, 255, 0);
-    // Wheels
+    //Upper Body
     glPushMatrix();
-    glTranslatef(0.0, 2.0, 0.0);
-    glutSolidTorus(0.5, 1.5, 20, 20);
+    glTranslatef(0.0625f, 0.24f, 0.0f);
+    glScalef(0.3125f, 0.16f, 0.0f);
+    glPushMatrix();
+    glColor3ub(0, 0, 255);
+    glTranslatef(1.0f, 1.0f, 0.0f);
+    glutSolidCube(1);
+    glPopMatrix();
+    glPopMatrix();
+
+    //Left Triangle
+    glPushMatrix();
+    glTranslatef(0.75f, 1.08f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glColor3ub(0, 0, 255);
+    glVertex3f(0, 0, 0);
+    glVertex3f(-0.125f, 0, 0);
+    glVertex3f(0, 0.16f, 0);
+    glEnd();
+    glPopMatrix();
+
+    //Right Triangle
+    glPushMatrix();
+    glTranslatef(1.375f, 1.08f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glColor3ub(0, 0, 255);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0.0625f, 0, 0);
+    glVertex3f(0, 0.16f, 0);
+    glEnd();
+    glPopMatrix();
+
+    //Left Wheel
+    glPushMatrix();
+    glColor3ub(0, 255, 0);
+    glTranslatef(0.625f, 0.72f, 0.0f);
+    glutSolidTorus(0.02, 0.0625, 20, 20);
+    glPopMatrix();
+
+    //Right Wheel
+    glPushMatrix();
+    glColor3ub(0, 255, 0);
+    glTranslatef(1.375f, 0.72f, 0.0f);
+    glutSolidTorus(0.02, 0.0625, 20, 20);
     glPopMatrix();
 }
 
