6b2557af940ca6413e236039edb3765e84c21076
VonWeidtfuderr <luis.ribeiro.gomes@tecnico.ulisboa.pt>
2014-10-05 21:43:50 +0100

    frog.cpp

--- src/frog.cpp
+++ src/frog.cpp
@@ -3,19 +3,49 @@
 
 void frog::leg::draw() {
     glPushMatrix();
-    glRotatef(25, 0.0, 0.0, 1.0);
+		glTranslatef(-0.2, 0.0, 0.0);
+    glRotatef(30, 0.0, 0.0, 1.0);
     glRotatef(20, 0.0, 1.0, 0.0);
-    glScalef(1.35, 0.35, 0.35);
+    glScalef(1.0, 0.45, 0.45);
     glutSolidCube(1.0);
     glPopMatrix();
 
     glPushMatrix();
-    glTranslatef(0.50, 0.0, 2.15);
+    glTranslatef(0.2, 0.0, 2.15);
     glRotatef(80, 0.0, 0.0, 1.0);
     glRotatef(-20, 0.0, 1.0, 0.0);
-    glScalef(0.75, -0.25, 0.25);
+    glScalef(0.75, -0.40, 0.40);
     glutSolidCube(1.0);
     glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.35, -0.5, 2.0);
+    glScalef(0.50, -0.35, 0.35);
+    glutSolidCube(1.0);
+    glPopMatrix();
+
+    glPushMatrix();
+		glTranslatef(1.15, 0.0, 0.0);
+    glRotatef(30, 0.0, 0.0, 1.0);
+    glRotatef(20, 0.0, 1.0, 0.0);
+    glScalef(1.0, 0.25, 0.25);
+    glutSolidCube(1.0);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(1.15, 0.0, 2.15);
+    glRotatef(80, 0.0, 0.0, 1.0);
+    glRotatef(-20, 0.0, 1.0, 0.0);
+    glScalef(0.75, -0.20, 0.20);
+    glutSolidCube(1.0);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(1.0, -0.5, 2.0);
+    glScalef(0.50, -0.15, 0.15);
+    glutSolidCube(1.0);
+    glPopMatrix();
+
 }
 
 void frog::leg::update(glut_time_t dt) {
@@ -25,21 +55,18 @@ void frog::leg::update(glut_time_t dt) {
 void frog::draw() {
     // Body
     glPushMatrix();
-    glColor3ub(0, 255, 0);
+    glColor3ub(5, 55, 5);
     glRotatef(10, 0.0, 0.0, 1.0);
-    glScalef(2.0, 0.5, 1.6);
+    glScalef(2.0, 0.75, 1.5);
     glutSolidCube(1.0);
     glPopMatrix();
 
     // Head
     glPushMatrix();
-    glColor3ub(255, 0, 0);
     glTranslatef(1.1, 0.55, 0);
     glScalef(1.0, 0.5, 1.0);
     glutSolidCube(1.0);
     glPopMatrix();
-
-    glColor3ub(0, 0, 255);
 
     // Left leg
     glPushMatrix();
