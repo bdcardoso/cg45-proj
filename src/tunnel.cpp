8cf522a76d42b9623b20d7ff0e996cf565892925
Bruno Cardoso <brunocardoso.lpma@gmail.com>
2014-10-18 18:47:16 +0100

    tunnel working

--- src/tunnel.cpp
+++ src/tunnel.cpp
@@ -6,8 +6,9 @@ constexpr auto SLICES =      20.0,
                
 constexpr auto RADIUS_0 =    1.0,
                RADIUS_Z =    1.0, 
-               COORD_X =     2.5,
-               COORD_Y =     1.0,
+               COORD_X =     2.3,
+               COORD_XNEG = -3.3,
+               COORD_Y =     -0.2,
                COORD_Z =     0.0;    
           
                
@@ -15,12 +16,27 @@ void tunnel::draw() {
 
     glColor3ub(50, 50, 50);
     
-    _scale.y(0.1);
     glPushMatrix();  
 		glTranslatef(COORD_X, COORD_Y, COORD_Z);
         glRotatef(90, 0.0, 1.0, 0.0);
-        //glScalef(RIVER_LENGTH, WATER_DEPTH, WATER_WIDTH);
         gluCylinder(gluNewQuadric(), RADIUS_0, RADIUS_Z, 1, SLICES, STACKS);
-     
-     glPopMatrix();  
+    glPopMatrix();  
+
+    glPushMatrix();  
+		glTranslatef(COORD_XNEG, COORD_Y, COORD_Z);
+        glRotatef(90, 0.0, 1.0, 0.0);
+        gluCylinder(gluNewQuadric(), RADIUS_0, RADIUS_Z, 1, SLICES, STACKS);     
+    glPopMatrix();  
+    
+        glPushMatrix();  
+		glTranslatef(COORD_X, COORD_Y, 2);
+        glRotatef(90, 0.0, 1.0, 0.0);
+        gluCylinder(gluNewQuadric(), RADIUS_0, RADIUS_Z, 1, SLICES, STACKS);
+    glPopMatrix();  
+
+    glPushMatrix();  
+		glTranslatef(COORD_XNEG, COORD_Y, 2);
+        glRotatef(90, 0.0, 1.0, 0.0);
+        gluCylinder(gluNewQuadric(), RADIUS_0, RADIUS_Z, 1, SLICES, STACKS);     
+    glPopMatrix();  
 }
