a79b2c6ea1708d9f29fc271a89aa0bbbab6ffb47
Rodrigo Lourenço <guigo.lourenco@gmail.com>
2014-11-08 14:24:52 +0000

    Sliced cube done.

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -9,12 +9,10 @@ void gluxInit() {}
 void gluxSlicedCube(int sx, int sy, int sz) {
     // srand(time(NULL));
     assert(sx > 0 && sx > 0 && sz > 0);
-    sx = sy = sz = 3;
 
     GLdouble dx = 1.0 / sx, dy = 1.0 / sy, dz = 1.0 / sz;
 
     // cross(y, z) = x
-    glColor3ub(255, 0, 0);
     for (int y = 0; y < sy; ++y) {
         GLdouble ny1 = y == 0 ? -1.0 : 0.0, ny2 = y == (sy - 1) ? 1.0 : 0.0;
 
@@ -44,7 +42,6 @@ void gluxSlicedCube(int sx, int sy, int sz) {
     }
 
     // cross(z, y) = -x
-    glColor3ub(0, 255, 0);
     for (int y = 0; y < sy; ++y) {
         GLdouble ny1 = y == 0 ? -1.0 : 0.0, ny2 = y == (sy - 1) ? 1.0 : 0.0;
 
@@ -74,7 +71,6 @@ void gluxSlicedCube(int sx, int sy, int sz) {
     }
 
     // cross(z, x) = y
-    glColor3ub(0, 0, 255);
     for (int x = 0; x < sx; ++x) {
         GLdouble nx1 = x == 0 ? -1.0 : 0.0, nx2 = x == (sx - 1) ? 1.0 : 0.0;
         for (int z = 0; z < sz; ++z) {
@@ -103,7 +99,6 @@ void gluxSlicedCube(int sx, int sy, int sz) {
     }
 
     // cross(x, z) = -y
-    glColor3ub(255, 255, 0);
     for (int x = 0; x < sx; ++x) {
         GLdouble nx1 = x == 0 ? -1.0 : 0.0, nx2 = x == (sx - 1) ? 1.0 : 0.0;
         for (int z = 0; z < sz; ++z) {
@@ -126,6 +121,64 @@ void gluxSlicedCube(int sx, int sy, int sz) {
             glNormal3f(nx1, -1.0, nz2);
             // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
             glVertex3f(-0.5 + (x + 0) * dx, -0.5, -0.5 + (z + 1) * dz);
+
+            glEnd();
+        }
+    }
+
+    // cross(x, y) = z
+    for (int x = 0; x < sx; ++x) {
+        GLdouble nx1 = x == 0 ? -1.0 : 0.0, nx2 = x == (sx - 1) ? 1.0 : 0.0;
+
+        for (int y = 0; y < sy; ++y) {
+            GLdouble ny1 = y == 0 ? -1.0 : 0.0, ny2 = y == (sy - 1) ? 1.0 : 0.0;
+
+            glBegin(GL_POLYGON);
+
+            glNormal3f(nx1, ny1, 1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 0) * dx, -0.5 + (y + 0) * dy, 0.5);
+
+            glNormal3f(nx2, ny1, 1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 1) * dx, -0.5 + (y + 0) * dy, 0.5);
+
+            glNormal3f(nx2, ny2, 1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 1) * dx, -0.5 + (y + 1) * dy, 0.5);
+
+            glNormal3f(nx1, ny2, 1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 0) * dx, -0.5 + (y + 1) * dy, 0.5);
+
+            glEnd();
+        }
+    }
+
+    // cross(y, x) = -z
+    for (int x = 0; x < sx; ++x) {
+        GLdouble nx1 = x == 0 ? -1.0 : 0.0, nx2 = x == (sx - 1) ? 1.0 : 0.0;
+
+        for (int y = 0; y < sy; ++y) {
+            GLdouble ny1 = y == 0 ? -1.0 : 0.0, ny2 = y == (sy - 1) ? 1.0 : 0.0;
+
+            glBegin(GL_POLYGON);
+
+            glNormal3f(nx1, ny1, -1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 0) * dx, -0.5 + (y + 0) * dy, -0.5);
+
+            glNormal3f(nx1, ny2, -1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 0) * dx, -0.5 + (y + 1) * dy, -0.5);
+
+            glNormal3f(nx2, ny2, -1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 1) * dx, -0.5 + (y + 1) * dy, -0.5);
+
+            glNormal3f(nx2, ny1, -1.0);
+            // glColor3ub(rand() % 256, rand() % 256, rand() % 256);
+            glVertex3f(-0.5 + (x + 1) * dx, -0.5 + (y + 0) * dy, -0.5);
 
             glEnd();
         }
